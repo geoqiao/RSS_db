71ede6841215ccd8db5a89469a28de366d3e6e33
geoqiao <realvincentqiao@gmail.com>
2024-03-14 23:03:51 +0800

    delete: remove app.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from utils import Feed, add_feed_to_db,get_all_feeds
+from utils import Feed, add_feed_to_db, get_all_feeds
 
 
 app = FastAPI()
@@ -21,11 +21,13 @@ async def add_feed_page(request: Request):
 @app.post("/addfeed")
 async def add_feed_submit(url: str = Form(...), tag: str = Form(...)):
     feed = Feed(url)
-    add_feed_to_db(url=url, title=feed.title, tag=tag,link=feed.link)
+    add_feed_to_db(url=url, title=feed.title, tag=tag, link=feed.link)
     return {"message": "add successfully"}
 
 
 @app.get("/feeds_list")
-async def get_rss_feeds(request:Request):
+async def get_rss_feeds(request: Request):
     feeds = get_all_feeds()
-    return template_dir.TemplateResponse("feeds_list.html",{"request":request,"feeds":feeds})
+    return template_dir.TemplateResponse(
+        "feeds_list.html", {"request": request, "feeds": feeds}
+    )
--- utils.py
+++ utils.py
@@ -1,14 +1,12 @@
 from typing import Dict, List, Optional
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column,Session
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 import feedparser
 
 
 engine = create_engine(
     "sqlite:///subscriptions.db", connect_args={"check_same_thread": False}
 )
-
-SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
 
 class Base(DeclarativeBase):
@@ -28,32 +26,25 @@ class Subscription(Base):
         return f"Feed(id={self.id!r}, url={self.url!r},title={self.title},tag={self.tag!r},link={self.link!r})"
 
 
-# class Subscription(Base):
-#     __tablename__ = "subscriptions"
-
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     url = Column(String, nullable=False, unique=True, index=True)
-#     title = Column(String)
-#     tag = Column(String)
-#     link = Column(String, index=True)
-
-
 Base.metadata.create_all(bind=engine)
 
 
-def add_feed_to_db(url: str, title: str, tag: str | None,link:str):
+def add_feed_to_db(url: str, title: str, tag: str | None, link: str):
     feed = Feed(url=url)
     with Session(engine) as session:
         session.add(Subscription(url=url, title=feed.title, tag=tag, link=feed.link))
         session.commit()
         session.close()
 
+
 def get_all_feeds():
     with Session(engine) as session:
         feeds = session.query(Subscription).all()
         return feeds
 
-print(get_all_feeds())
+
+# print(get_all_feeds())
+
 
 class Feed:
     def __init__(self, url: str, tag: str | None = None) -> None:
